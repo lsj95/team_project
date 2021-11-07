e7946c3f2821a5bcba3d9ef8f7f5f31b0cb410ed
duskite <duskite@github.com>
2021-11-07 23:18:44 +0900

    도별 병원수 데이터 함수 추가

--- load_data.py
+++ load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def load_covid(period=None):
+def load_covid(period=None): # 도별 확진환자수 데이터 로드
     missing_values = ['--', '-', 'na'] #nan값 설정
 
     df_covid = pd.read_csv('./resource/코로나바이러스감염증-19_확진환자_발생현황_211103.csv', index_col='일자',
@@ -20,8 +20,20 @@ def load_covid(period=None):
         tmp_df['기간 총합'] = tmp_df.sum(axis=1) # Warning 확인해볼 필요가 있음
         return tmp_df
 
-def load_hospital():
-    pass
 
+def load_hospital(): # 도별 병원수 데이터 로드
+    missing_values = ['--', '-', 'na']  # nan값 설정
 
+    df_hospital = pd.read_csv('./resource/건강보험심사평가원_의료기관별 감염병 및 중증환자 치료시설 현황_20210630.csv', index_col='시도명',
+                              na_values=missing_values)
+    df_hospital = df_hospital.groupby('시도명').agg(sum)
+    df_hospital['총 병상수'] = df_hospital.sum(axis=1)
+
+    # covid 데이터 프레임과 인덱스명 통일시킴
+    name_match = {'강원도':'강원','경기도':'경기','경상남도':'경남','경상북도':'경북','광주광역시':'광주','대구광역시':'대구',
+                  '대전광역시':'대전','부산광역시':'부산','서울특별시':'서울','세종특별자치시':'세종','울산광역시':'울산',
+                  '인천광역시':'인천','전라남도':'전남','전라북도':'전북','제주특별자치도':'제주','충청남도':'충남','충청북도':'충북'}
+    df_hospital = df_hospital.rename(index=name_match)
+
+    return df_hospital
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import font_init
 import matplotlib.pyplot as plt
-from load_data import load_covid
+from load_data import load_covid, load_hospital
 
 
 def main():
@@ -10,11 +10,16 @@ def main():
     # plt.bar(df_covid.index[1:], df_covid['누적(명)'][1:], align='center')
     # plt.show()
 
+    # 특정 날짜만 데이터 가져올때
     # df_covid = load_covid(['2020.1.20'])
     # print(df_covid)
 
+    #기간에 대한 데이터 가져올때 Warning은 확인 필요함
     df_covid = load_covid(['2020.1.20', '2020.2.23'])
     print(df_covid)
+
+    df_hospital = load_hospital()
+    print(df_hospital)
 
 
 main()
