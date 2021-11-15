ae6a0af8fdef63709f5c62dc567ed42ca308c5db
duskite <duskite@github.com>
2021-11-16 02:05:51 +0900

    테스트

--- load_data.py
+++ load_data.py
@@ -9,7 +9,7 @@ import xmltodict
 def load_covid(period=None): # 도별 확진환자수 데이터 로드
     missing_values = ['--', '-', 'na'] #nan값 설정
 
-    df_covid = pd.read_csv('./resource/코로나바이러스감염증-19_확진환자_발생현환_211103.csv', index_col='일자',
+    df_covid = pd.read_csv('./resource/코로나바이러스감염증-19_확진환자_발생현환_211103.csv', index_col='일자',
                            na_values=missing_values)
     df_covid = df_covid.transpose().fillna("0") # rows 지역, columns 일자로 전치함
     df_covid = df_covid.apply(lambda x: x.apply(lambda y:y.replace(",",""))).astype(np.int64) # 데이터프레임 내부의 모든값을 숫자로 바꿈
